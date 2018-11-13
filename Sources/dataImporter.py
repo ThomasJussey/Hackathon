6ea067bde36c30f431827c02c22ba8678e85697d
Julien NOWAK <julien.nowak@telecomnancy.eu>
2018-11-13 12:45:02 +0100

    Gestion des resultats

--- Sources/dataImporter.py
+++ Sources/dataImporter.py
@@ -5,7 +5,7 @@ import os
 import time
 debut = time.time()
 
-data = pickle.load( open( "../Data/data_2000/X_train.p", "rb" ))
+Xdata = pickle.load( open( "../Data/data_2000/X_train.p", "rb" ))
 
 
 #data[0] = couple d'image 1
@@ -24,11 +24,24 @@ for i in range(0,2000):
     if not os.path.exists("../Data/images/%d" % (i)):
         os.makedirs("../Data/images/%d" % (i))
         for j in range(0,2):
-                my_array = data[i][j].reshape((32, 32)).astype('uint8')
+                my_array = Xdata[i][j].reshape((32, 32)).astype('uint8')
                 im = Image.fromarray(my_array)
                 im.save("../Data/images/%d/result%d%d.jpg" % (i,i,j))
     else:
         print("This directory already exists")
+
+
+
+Ydata = pickle.load( open( "../Data/data_2000/Y_train.p", "rb" ))
+
+
+y_array = []
+for i in range(0,2000):
+    y_array.append(Ydata[i])
+    print(y_array[i] == 1)
+
+np.savetxt("../Data/images/results.txt", y_array)
+
 
 fin = time.time()
 print("Temps : %d sec" %(fin - debut))
