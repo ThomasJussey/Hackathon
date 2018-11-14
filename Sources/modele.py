e30388409f98fddf38541f821f77348695086229
Thomas Jussey <thomas.jussey@telecomnancy.eu>
2018-11-14 13:48:22 +0100

    Paramétrage du réseau.

--- Sources/modele.py
+++ Sources/modele.py
@@ -8,6 +8,8 @@ from keras.callbacks import TensorBoard
 import numpy.random as rng
 import numpy as np
 import os
+import sys
+import argparse
 import dill as pickle
 import tensorflow as tf
 from sklearn.utils import shuffle
@@ -15,96 +17,123 @@ import time
 
 debut = time.time()
 
-print ('#######################\n# LNet Neural Network #\n#######################\n')
+def main(args) :
+    print ('#######################\n# LNet Neural Network #\n#######################\n')
 
-#######################
-# Loading of the data #
-#######################
+    #######################
+    # Loading of the data #
+    #######################
 
-#Training Data
+    #Training Data  
 
-print ('Loading of the training data...')
+    print ('Loading of the training data...')
 
-X_train = pickle.load( open( "../Data/data_2000/X_train.p", "rb" ))
-Y_train = pickle.load( open( "../Data/data_2000/y_train.p", "rb" ))
+    X_train = pickle.load( open(  args.data_dir + "X_train.p", "rb" ))
+    Y_train = pickle.load( open(  args.data_dir + "y_train.p", "rb" ))
 
-left_data = []
-right_data = []
+    left_data = []
+    right_data = []
 
-n = len(X_train)
-for i in range(n) :
-    left_data.append(X_train[i][0])
-    right_data.append(X_train[i][1])
+    n = len(X_train)
+    for i in range(n) :
+        left_data.append(X_train[i][0])
+        right_data.append(X_train[i][1])
 
-print ('Done.\nThere are {} examples for training.\n'.format(Y_train.shape[0]))
+    print ('Done.\nThere are {} examples for training.\n'.format(Y_train.shape[0]))
 
-# Validation data
+    # Validation data
 
-print ('Loading of the validation data...')
+    print ('Loading of the validation data...')
 
-X_val = pickle.load( open( "../Data/data_2000/X_val.p", "rb" ))
-Y_val = pickle.load( open( "../Data/data_2000/y_val.p", "rb" ))
+    X_val = pickle.load( open( args.data_dir + "X_val.p", "rb" ))
+    Y_val = pickle.load( open(  args.data_dir + "y_val.p", "rb" ))
 
-validation_left = []
-validation_right = []
+    validation_left = []
+    validation_right = []
 
-n = len(X_val)
-for i in range(n):
-    validation_left.append(X_val[i][0])
-    validation_right.append(X_val[i][1])
+    n = len(X_val)
+    for i in range(n):
+        validation_left.append(X_val[i][0])
+        validation_right.append(X_val[i][1])
 
-print ('Done.\nThere are {} examples for validation.\n'.format(Y_val.shape[0]))
+    print ('Done.\nThere are {} examples for validation.\n'.format(Y_val.shape[0]))
 
-#Tensoboard initalization
-tensorboard = TensorBoard(log_dir='../logs', histogram_freq=0,
-          write_graph=True, write_images=True)
+    ##################################
+    # Creation of the Neural Network #
+    ##################################
 
-# We have 2 inputs, 1 for each picture, we are currently working with Black and White images in 32x32 pixels.
-left_input = Input((32,32,1))
-right_input = Input((32,32,1))
+    #Tensoboard initalization
+    tensorboard = TensorBoard(log_dir='../logs', histogram_freq=0,
+                              write_graph=True, write_images=True)
 
-# We will use 2 instances of 1 LNet network for this task
-convnet = Sequential([
-    Conv2D(5,3,activation='relu',input_shape=(32,32,1)),
-    MaxPooling2D(),
-    Conv2D(5,3,activation='relu'),
-    MaxPooling2D(),
-    Conv2D(7,2,activation='relu'),
-    MaxPooling2D(),
-    Conv2D(7,2,activation='relu'),
-    Flatten(),
-    Dense(18, activation="sigmoid"),
-])
 
-# Connect each 'leg' of the network to each input
-# Remember, they have the same weights
-encoded_l = convnet(left_input)
-encoded_r = convnet(right_input)
+    # We have 2 inputs, 1 for each picture, we are currently working with Black and White images in 32x32 pixels.
+    size = args.image_size
+    dim = args.image_dim
+    left_input = Input((size,size,dim))
+    right_input = Input((size,size,dim))
 
-# Getting the L1 Distance between the 2 encodings
-L1_layer = Lambda(lambda tensor:K.abs(tensor[0] - tensor[1]))
+    # We will use 2 instances of 1 LNet network for this task
+    convnet = Sequential([
+        Conv2D(5,3,activation='relu',input_shape=(32,32,1)),
+        MaxPooling2D(),
+        Conv2D(5,3,activation='relu'),
+        MaxPooling2D(),
+        Conv2D(7,2,activation='relu'),
+        MaxPooling2D(),
+        Conv2D(7,2,activation='relu'),
+        Flatten(),
+        Dense(18, activation="sigmoid"),
+    ])
 
-# Add the distance function to the network
-L1_distance = L1_layer([encoded_l, encoded_r])
+    # Connect each 'leg' of the network to each input
+    # Remember, they have the same weights
+    encoded_l = convnet(left_input)
+    encoded_r = convnet(right_input)
 
-prediction = Dense(1,activation='sigmoid')(L1_distance)
-siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
+    # Getting the L1 Distance between the 2 encodings
+    L1_layer = Lambda(lambda tensor:K.abs(tensor[0] - tensor[1]))
 
-optimizer = Adam(0.001, decay=2.5e-4)
-#//TODO: get layerwise learning rates and momentum annealing scheme described in paperworking
-siamese_net.compile(loss="binary_crossentropy",optimizer=optimizer,metrics=['accuracy'])
+    # Add the distance function to the network
+    L1_distance = L1_layer([encoded_l, encoded_r])
 
-# Tensorboard
+    prediction = Dense(1,activation='sigmoid')(L1_distance)
+    siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
 
-# Training and validation
-siamese_net.summary()
-siamese_net.fit([left_data,right_data], Y_train,
-          batch_size=16,
-          epochs=100,
-          verbose=2,
-          validation_data=([validation_left,validation_right],Y_val),
-          shuffle=True,
-          callbacks=[tensorboard])
+    optimizer = Adam(0.001, decay=2.5e-4)
+    #//TODO: get layerwise learning rates and momentum annealing scheme described in paperworking
+    siamese_net.compile(loss="binary_crossentropy",optimizer=optimizer,metrics=['accuracy'])
 
-fin = time.time()
-print("Temps : %f sec" %(fin - debut))
+    ###########################
+    # Training and validation #
+    ###########################
+
+    siamese_net.summary()
+    siamese_net.fit([left_data,right_data], Y_train,
+            batch_size=16,
+            epochs=args.epochs,
+            verbose=2,
+            validation_data=([validation_left,validation_right],Y_val),
+            shuffle=args.shuffle,
+            callbacks=[tensorboard])
+
+    fin = time.time()
+    print("Temps : %f sec" %(fin - debut))
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--data_dir', type=str, help='Directory with the training and the validation sets.', default='../Data/data_2000/')
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=32)
+    parser.add_argument('--image_dim', type=int,
+        help='Image dimension (colors) 1 for black and white and 3 for rgb.', default=1)
+    parser.add_argument('--epochs', type=int,
+        help='Number of epochs.', default=30)
+    parser.add_argument('--shuffle', 
+        help='Shuffle the datasets at each epoch.', action='store_true')
+    return parser.parse_args(argv)
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
